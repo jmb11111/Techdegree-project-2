fdc3ea7aa1fc3f17760c37152c3274b3f971aff4
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-02-19 14:41:29 -0500

    added dynamic ID's to pagination links

--- pagination-filter-v2/js/script.js
+++ pagination-filter-v2/js/script.js
@@ -1,26 +1,28 @@
 const students = $("li.student-item");
 let pagination = $('.pagination')
 let pages= Math.ceil(students.length/10);
-
-const createPagination = function createPagination(){
-  let ul=document.createElement('ul');
-     for(i=0;i<pages;i++)
-     {
-         var li=document.createElement('li');
-         li.innerHTML= "<a href=#>"+(i+1)+ "</a>";
-         ul.appendChild(li);
-     }
-     document.getElementById('pagination').appendChild(ul);
-}
-
+//function that hides all students
 const hideStudents = function hideStudents(){
   for (let i = 0; i < students.length; i++) {
     students[i].style.display='none';
   }
 }
-
+//function that shows first ten students
 const showStudents = function showStudents(){
   for (let i = 0; i < 10; i++) {
     students[i].style.display=''
   }
 }
+const createPagination = function createPagination(){
+  let ul=document.createElement('ul');
+     for(i=0;i<pages;i++)
+     {
+         let li=document.createElement('li');
+         li.innerHTML= "<a href=# class='pages'"+ 'id='+(i+1)+ ">"+(i+1)+"</a>";
+         let pageNumber= document.getElementsByClassName('pages').id = i+1;
+         ul.appendChild(li);
+     }
+     document.getElementById('pagination').appendChild(ul);
+
+}
+createPagination();
