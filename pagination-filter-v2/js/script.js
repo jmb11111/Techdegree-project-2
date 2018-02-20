2557d588268c364a838fd45438f8d28dfa277678
Joshua Blasbalg <joshua.blasbalg@gmail.com>
2018-02-20 15:43:01 -0500

    added non operational search button and input field

--- pagination-filter-v2/js/script.js
+++ pagination-filter-v2/js/script.js
@@ -4,6 +4,8 @@ const students = $("li.student-item");
 let pages= Math.ceil(students.length/10);
 
 
+
+
 //function that hides all students
 function hideStudents(){
   for (let i = 0; i < students.length; i++) {
@@ -22,18 +24,23 @@ let $students = students.slice((x-1) * 10, x * 10);
 //creates buttons on bottom of page depending on how many items are in the students array, then calls load students passing in the argument based on the number pressed
 function createPagination(){
   let ul=document.createElement('ul');
-
-     for(i=0;i<pages;i++)
+       for(i=0;i<pages;i++)
      {
          let li=document.createElement('li');
          li.innerHTML= "<button onclick= loadStudents("+(i+1)+") class='pages'"+'>'+(i+1)+ "</button>";
          ul.appendChild(li);
-
      }
      document.getElementById('pagination').appendChild(ul);
 }
 
-
-
+function createSearch(){
+  let search=document.createElement('input');
+  let searchButton=document.createElement('button');
+    search.type = "text";
+    search.placeholder = "Search for students...";
+    searchButton.innerHTML="Search";
+document.getElementById('student-search').appendChild(search);
+document.getElementById('student-search').appendChild(searchButton);
+}
 loadStudents(1);
 createPagination();
